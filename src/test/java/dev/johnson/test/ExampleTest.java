295dedef101c165367eaef42ec0314e11ab155ac
elliej1994 <102549808+elliej1994@users.noreply.github.com>
2022-04-21 11:01:43 -0400

    updated pom

---
+++ src/test/java/dev/johnson/test/ExampleTest.java
@@ -0,0 +1,11 @@
+package dev.johnson.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void should_pass(){
+        Assertions.assertEquals(1,1);
+    }
+}
